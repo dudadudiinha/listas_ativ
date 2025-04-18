c72262ebb881ba4da8013a3d23c6e554bc4d7dd1
dudadudiinha <dudamoura.n@gmail.com>
2025-04-18 16:06:00 +0000

    faltam quatro questões para finalizar a lista04

--- listas/lista04.py
+++ listas/lista04.py
@@ -11,7 +11,6 @@ else:
     r2 = (-b-(delta)**(1/2))/(2*a)
     print(f'R1 = {r1:.5f}\nR2 = {r2:.5f}')
 
-
 #1044
 nume = input('Digite dois números: ').split()
 n1 = float(nume[0])
@@ -19,14 +18,54 @@ n2 = float(nume[1])
 if n1%n2 == 0 or n2%n1 == 0:
     print('São múltiplos')
 else:
-    print('Não são múltiplos')'''
+    print('Não são múltiplos')
 
 #1049
-
+animal = {
+    "vertebrado": {
+        "ave": {
+            "carnivoro": "aguia",
+            "onivoro": "pomba"
+        },
+        "mamifero": {
+            "onivoro": "homem",
+            "herbivoro": "vaca"
+        }
+    },
+    "invertebrado": {
+        "inseto": {
+            "hematofago": "pulga",
+            "herbivoro": "lagarta"
+        },
+        "anelideo": {
+            "hematofago": "sanguessuga",
+            "onivoro": "minhoca"
+        }
+    }
+}
+classif1 = input('Vertebrado ou invertebrado? ').lower()
+classif2 = input('Qual a classe dele? ').lower()
+classif3 = input('Qual a sua alimentação? ').lower()
+print(animal[classif1][classif2][classif3])
 
 #1050
+ddd = int(input('Digite seu DDD: '))
+ddds = {
+    61: "Brasília",
+    71: "Salvador",
+    11: "São Paulo",
+    21: "Rio de Janeiro",
+    32: "Juiz de Fora",
+    19: "Campinas",
+    27: "Vitória",
+    31: "Belo Horizonte"
+}
+if ddd in ddds:
+    print(ddds[ddd])
+else:
+    print("DDD não cadastrado")
 
-'''#2424
+#2424
 numbers = input('Digite as coordenadas em que a bola bateu: ').split()
 x = int(numbers[0])
 y = int(numbers[1])
@@ -42,8 +81,29 @@ a3 = int(input('Digite o número de pessoas no terceiro piso: '))
 primeiro_andar = a3*4 + a2*2
 segundo_andar = a3*2 + a1*2
 terceiro_andar = a1*4 + a2*2
-print(f'{min(primeiro_andar, segundo_andar, terceiro_andar)} minutos')'''
+print(f'{min(primeiro_andar, segundo_andar, terceiro_andar)} minutos')
 
 #1059
+for i in range(2, 100+1, 2):
+    print(i)
+
+#1080
+maior = 0
+posicao = 0
+for i in range(1, 100+1):
+    num = int(input('Digite um número: '))
+    if num > maior:
+        maior = num
+        posicao = i
+print(f'O maior número é {maior}, que se encontra na posição {posicao}')'''
+
+#1094
+
+
+#1114
+
+
+#1116
+
 
 #1151
